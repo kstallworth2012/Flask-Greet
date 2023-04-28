5a23c6bad46dec8259ddb85f65d9fc648396ba07
kstallworth2012 <kennethstallworth@hotmail.com>
2023-04-28 23:53:40 +0000

    Initial commit

--- app.py
+++ app.py
@@ -5,3 +5,27 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/welcome")
+def welcome():
+    """
+    /welcome
+    Returns “welcome”
+    """
+    return "<h1>Welcome</h1>"
+
+@app.route("/welcome/home")
+def home():
+    """
+    /welcome/home
+    Returns “welcome home”
+    """
+    return "<h1>Welcome Home</h1>"
+
+@app.route("/welcome/back")
+def welcome_back():
+    """
+    /welcome/back
+    Return “welcome back”
+    """
+    return "<h1>Welcome back</h1>"
